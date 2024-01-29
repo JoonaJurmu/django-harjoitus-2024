2a352e33073cab37a7957b02d554026a380419f2
JoonaJurmu <156064173+JoonaJurmu@users.noreply.github.com>
2024-01-29 15:01:50 +0200

    Kysely-sovelluksen models

---
+++ kysely/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Kysymys(models.Model):
+    teksti = models.CharField(max_length=200)
+    julkaisupvm = models.DateTimeField("julkaistu")
+
+
+class Vaihtoehto(models.Model):
+    question = models.ForeignKey(Kysymys, on_delete=models.CASCADE)
+    teksti = models.CharField(max_length=200)
+    ääniä = models.IntegerField(default=0)
+
+
