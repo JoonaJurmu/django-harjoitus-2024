3096c1a8591b1005de9c06ba977c081a4f847d35
JoonaJurmu <156064173+JoonaJurmu@users.noreply.github.com>
2024-02-12 10:51:28 +0200

    Lisää onko_julkaistu_lähiaikoina metodi

--- kysely/models.py
+++ kysely/models.py
@@ -1,5 +1,6 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 
 class Kysymys(models.Model):
     teksti = models.CharField(max_length=200)
@@ -11,6 +12,12 @@ class Kysymys(models.Model):
 
     def __str__(self):
         return self.teksti
+
+    def onko_julkaistu_lähiaikoina(self):
+        return self.julkaisupvm >= timezone.now() - datetime.timedelta(days=1)
+
+kysymys = Kysymys()
+kysymys.onko_julkaistu_lähiaikoina()
 
 
 class Vaihtoehto(models.Model):
